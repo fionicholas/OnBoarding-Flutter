a77b741065d6549d419eb132acc05c6e0490baa6
fionicholas <fiorentcheaters@gmail.com>
2020-09-27 22:43:41 +0700

    add routes for OTP screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,11 @@
 
 import 'package:flutter/material.dart';
 import 'package:on_boarding/onboarding/onboarding_screen.dart';
+import 'package:on_boarding/otp/otp_screen.dart';
 import 'package:on_boarding/signin/signin_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   OnBoardingScreen.routeName: (context) => OnBoardingScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
+  OTPScreen.routeName: (context) => OTPScreen(),
 };
